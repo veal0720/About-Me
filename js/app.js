1b14b88aa9ef4594fa017ba3f4650082913b1747
tektechnologies <info@tektechnologies.com>
2022-11-05 14:19:41 -0500

    add user name input

--- js/app.js
+++ js/app.js
@@ -2,3 +2,13 @@
 console.log('app js connected');
 
 //global variables
+
+
+
+
+let userName = prompt('Welcome to my guessing game. Whats your name?');
+
+while(!userName){
+  userName = prompt('No, really. I need a name, thanks.');
+  console.log(userName);
+}
